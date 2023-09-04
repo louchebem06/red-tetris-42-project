3114c0c733db5cbe7134ecb30f0f6c38757deae4
bledda <bledda@student.42nice.fr>
2023-09-04 19:47:08 +0200

    scss on svelte
    This feature adding sccs on frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+					@use '$lib/scss/variables' as *;    
+					@use '$lib/scss/mixins' as *;
+				`,
+			},
+		},
+	},
 });
