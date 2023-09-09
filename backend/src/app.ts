ffa767487f92236a7cd829500bf9def6e5d6cc9c
AudeizReading <alellouc@student.42nice.fr>
2023-09-09 13:32:39 +0200

    gestion username ok
    numerote correctement les usernames.
    intercale les numerotations si un se deco
    remove toutes les references a l'username root.
    redonne un username root deconnecté et reajuste les ref dessus si necessaires pour pouvoir reprendre la numerotation et le suivi correctement
    changement de pseudo ok, si le username a la même racine que le old username, on ne fait rien, sinon on update;

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,84 +22,6 @@ const io = new Server(server);
 
 const users: IPlayer[] = [];
 
-/*class UsernamesList {
-	static datas: Map<string, number> = new Map();
-
-	static setRecurrence(value: string, recurrence: number): void {
-		if (value && recurrence) {
-			this.datas.set(value, recurrence);
-		}
-	}
-
-	static addRecurrence(value: string): void {
-		if (this.hasRecurrence(value)) {
-			const recurrence = this.getRecurrence(value);
-			this.setRecurrence(value, recurrence + 1);
-		} else {
-			this.setRecurrence(value, 1);
-		}
-	}
-
-	static removeRecurrence(value: string): boolean {
-		if (this.hasRecurrence(value)) {
-			const recurrence = this.getRecurrence(value);
-			if (recurrence > 1) {
-				this.setRecurrence(value, recurrence - 1);
-				return true;
-			} else {
-				this.deleteRecurrence(value);
-			}
-		}
-		return false;
-	}
-
-	static getRecurrence(value: string): number {
-		if (this.hasRecurrence(value)) {
-			return this.datas.get(value) || 0;
-		}
-		return 0;
-	}
-
-	static hasRecurrence(value: string): boolean {
-		if (this.datas?.has(value))
-			return true;
-		return false;
-	}
-
-	static deleteRecurrence(value: string): void {
-		console.log(`deleteRecurrence: ${value}`);
-		if (this.hasRecurrence(value)) {
-			this.datas.delete(value);
-			console.log(`deleted: ${value} - ${this.hasRecurrence(value)}`);
-			if (this.datas.size === 0) {
-				this.datas.clear();
-				console.log(`all clear: ${value} - ${this.hasRecurrence(value)}`);
-			}
-		}
-	}
-
-	static getNewUsername(value: string): string {
-		if (!this.hasRecurrence(value)) {
-			this.addRecurrence(value);
-			return value;
-		}
-
-		let newUsername = value;
-		while (this.hasRecurrence(newUsername)) {
-			const recurrence = this.getRecurrence(newUsername);
-
-			newUsername = value + "#" + recurrence.toString();
-			this.addRecurrence(value);
-		}
-		this.addRecurrence(newUsername);
-		return newUsername;
-	}
-
-	static get list(): string[] {
-		return Array.from(this.datas.keys()).map((key: string) => key) || [];
-	}
-}*/
-
 function removeUser(socketId: string, users: IPlayer[]): boolean {
    const player = users.find(user => user.socketId === socketId);
 
@@ -110,15 +32,7 @@ function removeUser(socketId: string, users: IPlayer[]): boolean {
 
    if (username) {
 	   console.log(`${username} is leaving us.`);
-	   const sharpIdx = username.indexOf("#");
-
 	   UsernamesList.removeRecurrence(username);
-	   if (sharpIdx !== -1) {
-		   const root = username.substring(0, sharpIdx);
-		   if (UsernamesList.hasRecurrence(root)) {
-			   UsernamesList.removeRecurrence(root);
-		   }
-	   }
    }
    users.splice(users.indexOf(player), 1);
    Player.decremente();
@@ -131,14 +45,22 @@ function getNbUsersInfo(): string {
 	const s = `${Player.getNbActivePlayers() < 2 ? "" : "s"}`;
 	return `There ${be} ${Player.getNbActivePlayers()} player${s} on the server`;
 }
+
 function getConnectionUserInfos(username: string, socketId: string): string {
 	let msg = `${username} has just joined on ${socketId} socket`;
 	msg += "\n" + getNbUsersInfo();
 	return msg;
 }
 
+function debugPlayers(users: IPlayer[]): void {
+
+	const _users = JSON.stringify(users);
+	console.log(`\x1b[33musers\x1b[0m: ${_users}\n\x1b[33musernames\x1b[0m:`);
+	UsernamesList.display();
+}
+
 io.on('connection', (socket: Socket) => {
-	console.log(`Socket \x1b[96;4m${socket.id}\x1b[0m is connected`);
+//  console.log(`Socket \x1b[96;4m${socket.id}\x1b[0m is connected`);
   
   socket.on("join", (data: {username?: string, id?: string}) => {
 	 const { username, id } = data;
@@ -152,25 +74,19 @@ io.on('connection', (socket: Socket) => {
 			|| new Player(socket.id);
 
 		if (users.indexOf(player) === -1) {
-			player.username = UsernamesList.getNewUsername(username || "anon");
+			player.username = UsernamesList.setNewUsername(username || "anon");
 			users.push(player);
-			console.log(player, users);
 		} else if (username && player.username) {
-			const oldUsername = player.username;
-			if (oldUsername !== username) {
-				player.username = UsernamesList.getNewUsername(username) || oldUsername;
-				UsernamesList.removeRecurrence(oldUsername);
-			}
+			player.username = UsernamesList.updateUsername(player.username, username);
 		}
-		console.log(`${getConnectionUserInfos(player.username || "", player.socketId || "")}`);
-		io.emit("join", {username: player.username, id: player.socketId});
+		//console.log(`${getConnectionUserInfos(player.username || "", player.socketId || "")}`);
+		//io.emit("join", {username: player.username, id: player.socketId});
+		socket.emit("join", {username: player.username, id: player.socketId});
 	 }
   });
 
-  socket.on('disconnect', (data) => {
+  socket.on('disconnect', () => {
 	  removeUser(socket.id, users);
-	  console.log(`Socket \x1b[96;4m${socket.id}\x1b[0m is disconnected`, 
-	  			  Player.getNbActivePlayers(), data);
   });
 });
 
--- backend/src/model/UsernamesList.ts
+++ backend/src/model/UsernamesList.ts
@@ -9,22 +9,35 @@ class UsernamesList {
 
 	static addRecurrence(value: string): void {
 		if (this.hasRecurrence(value)) {
-			const recurrence = this.getRecurrence(value);
+			let recurrence = this.getRecurrence(value);
+			if (this.isAvailableRootUsername(value)) {
+				const root = value + "#";
+				let recRoot = 0;
+				console.log()
+				for (;this.hasRecurrence(root + (recRoot + 1).toString());recRoot++)
+				if (this.hasRecurrence(root + (recRoot).toString()))
+					++recRoot
+				recurrence += recRoot;
+			}
 			this.setRecurrence(value, recurrence + 1);
 		} else {
-			this.setRecurrence(value, 1);
+			this.setRecurrence(value, 1)
 		}
 	}
 
 	static removeRecurrence(value: string): boolean {
 		if (this.hasRecurrence(value)) {
-			const recurrence = this.getRecurrence(value);
-			if (recurrence > 1) {
-				this.setRecurrence(value, recurrence - 1);
-				return true;
-			} else {
-				this.deleteRecurrence(value);
+			const root = this.getRootUsername(value);
+			if (!this.isRootUsername(value) && this.hasRecurrence(root)) {
+				const recurrence = this.getRecurrence(root);
+				if (recurrence > 1) {
+					this.setRecurrence(root, recurrence - 1);
+				} else {
+					this.removeRecurrence(root);
+				}
 			}
+			this.deleteRecurrence(value);
+			return true;
 		}
 		return false;
 	}
@@ -43,36 +56,99 @@ class UsernamesList {
 	}
 
 	static deleteRecurrence(value: string): void {
-		console.log(`deleteRecurrence: ${value}`);
 		if (this.hasRecurrence(value)) {
 			this.datas.delete(value);
-			console.log(`deleted: ${value} - ${this.hasRecurrence(value)}`);
 			if (this.datas.size === 0) {
 				this.datas.clear();
-				console.log(`all clear: ${value} - ${this.hasRecurrence(value)}`);
 			}
 		}
 	}
 
-	static getNewUsername(value: string): string {
+	static setNewUsername(value: string): string {
 		if (!this.hasRecurrence(value)) {
+			const root = value + "#";
+			this.datas.forEach((val, key) => {
+				if (key.includes(root)) {
+					this.addRecurrence(value)
+				}
+			});
 			this.addRecurrence(value);
+			return value;
+		} else {
+			let newUsername = value;
+			let recurrence = 1;
+			while (this.hasRecurrence(newUsername)) {
+				newUsername = value + "#" + recurrence.toString();
+				recurrence++;
+			}
+			if (value !== newUsername)
+				this.addRecurrence(value);
+			this.addRecurrence(newUsername);
+			return newUsername;
+		}
+	}
+
+	static updateUsername(old: string, newValue: string): string {
+		const oldRoot = this.getRootUsername(old);
+		if (newValue !== oldRoot || this.isAvailableRootUsername(oldRoot)) {
+			this.removeRecurrence(old);
+			return this.setNewUsername(newValue);
+		}
+		return old;
+	}
+
+	/*static getNewUsername(value: string): string {
+		if (!this.hasRecurrence(value)) {
 			return value;
 		}
 
 		let newUsername = value;
+		let recurrence = this.getRecurrence(newUsername);
 		while (this.hasRecurrence(newUsername)) {
-			const recurrence = this.getRecurrence(newUsername);
-
 			newUsername = value + "#" + recurrence.toString();
-			this.addRecurrence(value);
+			recurrence++;
 		}
-		this.addRecurrence(newUsername);
 		return newUsername;
+	}*/
+
+	static isRootUsername(value: string): boolean {
+		return !value.includes("#");
+	}
+
+	static isAvailableRootUsername(value: string): boolean {
+		return this.isRootUsername(value) && !this.hasRecurrence(value);
+	}
+
+	static getRootUsername(value: string): string {
+		if (this.isRootUsername(value))
+			return value;
+		return value.substring(0, value.indexOf("#"));
 	}
 
 	static get list(): string[] {
 		return Array.from(this.datas.keys()).map((key: string) => key) || [];
+	}
+
+	static display(): void {
+		const it = this.datas.entries();
+		for (const i of it) {
+			console.log(`username: ${i}`);
+		}
+		console.log(`total usernames: ${this.datas.size}`);
+		const values = [...this.datas.values()];
+		if (values && values.length > 1)
+			console.log(`total recurrences: ${values.reduce((a, b) => a + b)}`);
+	}
+
+	static displayUsername(value: string): void {
+		if (this.hasRecurrence(value)) {
+			const it = this.datas.keys();
+			for (const i of it) {
+				if (i === value) {
+					console.log(`username: ${i}, recurrence: ${this.getRecurrence(value)}`);
+				}
+			}
+		}
 	}
 }
 
