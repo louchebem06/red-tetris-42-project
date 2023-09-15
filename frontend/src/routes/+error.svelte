367e73581d056e03f682854aeffc6e7e1a95b930
bledda <bledda@student.42nice.fr>
2023-09-16 00:21:48 +0200

    :sparkles: Error Page
    :sparkles: create error page
    :bug: Ajustement KeysGame and Sound
    :sparkles: add test for error page
    :sparkles: add timeout for tests
    :sparkles: ignore console error in test

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.status} - {$page.error?.message} | Red Tetris</title>
+</svelte:head>
+
+<div>
+	<h1>{$page.status}</h1>
+	<p>{$page.error?.message}</p>
+	<a href="/">Comme back to Home</a>
+</div>
+
+<style lang="scss">
+	div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		height: 100vh;
+		color: $white2;
+
+		h1 {
+			font-size: 100px;
+		}
+
+		p {
+			font-size: 30px;
+		}
+
+		a {
+			font-size: 24px;
+			color: $white2;
+		}
+	}
+</style>
