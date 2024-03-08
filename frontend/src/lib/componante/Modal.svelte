ba0dd69b7d8b07bf98c4b32ea1b42dc04be8627e
Bryan Ledda <bledda@student.42nice.fr>
2024-03-08 04:43:12 +0100

    fix: display content modal
    add background if modal open
    add close with escape keyboard

--- frontend/src/lib/componante/Modal.svelte
+++ frontend/src/lib/componante/Modal.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	import Button from '$lib/componante/Button.svelte';
+	import { onMount } from 'svelte';
 	export let show: boolean;
+
+	onMount(() => {
+		document.addEventListener('keydown', closeOnEscape);
+		return (): void => document.removeEventListener('keydown', closeOnEscape);
+	});
+
+	const closeOnEscape = (e: KeyboardEvent): void => {
+		if (e.code != 'Escape') return;
+		close();
+	};
 
 	const close = (): void => {
 		show = false;
@@ -8,9 +19,12 @@
 </script>
 
 {#if show}
+	<button on:click={close} class="background" />
 	<div class="modal">
 		<Button class="button" on:click={close}>Close</Button>
-		<slot />
+		<div class="content">
+			<slot />
+		</div>
 	</div>
 {/if}
 
@@ -28,10 +42,27 @@
 		border-radius: 10px;
 		box-shadow: rgba(255, 162, 0, 0.2) 0px 7px 29px 0px;
 
+		.content {
+			margin-top: 15px;
+		}
+
 		:global(.button) {
 			position: fixed;
 			top: 10px;
 			right: 10px;
 		}
+	}
+
+	.background {
+		width: 100vw;
+		height: 100vh;
+		background: $yellow;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 9;
+		opacity: 0.5;
+		border: none;
+		cursor: default;
 	}
 </style>
